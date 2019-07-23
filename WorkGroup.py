3b12eeda099d45a8b2f92d56c3249d7932efb30c
Екатерина Симонова <kate.sim@maas.agency>
2019-07-23 19:50:19 +0300

    object to WorkGroup

--- WorkGroup.py
+++ WorkGroup.py
@@ -1,17 +1,14 @@
 from DBController import DBController
-import uuid
 
 
-class WorkGroup:
-    def __init__(self, channel='DHCLCG8DQ', users=['UHTUFSFN2'], times='7:30'):
-        self.channel = channel
-        self.users = users
-        self.direct_id = None
-        self.times = times
-        self.reports = []
-        self.ts_reports = None
-        DBController.first_setup()
-        DBController.add_group(group=self.serialize(), uuid=str(uuid.uuid4()))
+class WorkGroup(object):
+    def __init__(self, obj: dict):
+        self.channel = obj.get('channel', None)
+        self.users = obj.get('users', None)
+        self.direct_id = obj.get('direct_id', None)
+        self.times = obj.get('times', None)
+        self.reports = obj.get('reports', [])
+        self.ts_reports = obj.get('ts_reports', None)
 
     def serialize(self):
         return dict(channel=self.channel,
@@ -24,14 +21,15 @@ class WorkGroup:
     def update_reports(self, channel, report, ts_reports):
         if len(self.reports) == 0:
             self.reports.append(report)
+        self.ts_reports = ts_reports
 
         for obj in self.reports:
             if obj.get('id_user') == report.get('id_user'):
                 obj['answers'] = report['answers']
             else:
                 self.reports.append(report)
-        DBController.update_reports(channel, self.reports, ts_reports)
-        return self.reports
+        DBController.update_reports(channel, self.reports, self.ts_reports)
 
     def set_direct_id(self, direct_id):
         self.direct_id = direct_id
+
