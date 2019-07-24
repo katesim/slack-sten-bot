6363dfeb2a5a6283947ab1532b06499358759035
Екатерина Симонова <kate.sim@maas.agency>
2019-07-24 19:24:42 +0300

    Upd model of Report

--- WorkGroup.py
+++ WorkGroup.py
@@ -6,7 +6,7 @@ class WorkGroup(object):
         self.channel = obj.get('channel', None)
         self.users = obj.get('users', None)
         self.times = obj.get('times', None)
-        self.reports = obj.get('reports', [])
+        self.reports = obj.get('reports', {})
         self.ts_reports = obj.get('ts_reports', None)
 
     def serialize(self):
@@ -17,13 +17,6 @@ class WorkGroup(object):
                     ts_reports=self.ts_reports)
 
     def update_reports(self, report, ts_reports):
-        if not self.reports:
-            self.reports.append(report)
+        self.reports.update(report)
         self.ts_reports = ts_reports
-
-        for obj in self.reports:
-            if obj.get('id_user') == report.get('id_user'):
-                obj['answers'] = report['answers']
-            else:
-                self.reports.append(report)
         DBController.update_reports(self.channel, self.reports, self.ts_reports)
